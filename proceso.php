a17671f5855beee459b8b7eaceb9253a04c642aa
jhondominguez1 <jjjdomi@gmail.com>
2016-06-03 04:47:33 -0500

    adicionado desde el equipo con usuario jhondominguez1

---
+++ proceso.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "este archivo se agrega desde pc jhondominguez1";
+
+?>
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
